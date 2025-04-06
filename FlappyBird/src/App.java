c73ca315815a625ce24a8d912e95ef8d1b3fd065
Daniel Garrido <danielgc81@educastur.es>
2025-04-06 14:28:39 +0200

    Juego acabado

--- FlappyBird/src/App.java
+++ FlappyBird/src/App.java
@@ -1,5 +1,20 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+   public static void main(String[] args) throws Exception {
+      int boardWidth = 360;
+      int boardHeight = 640;
+
+      JFrame frame = new JFrame("Flappy Bird");
+      frame.setSize(boardWidth, boardHeight);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(false);
+
+      FlappyBird flappyBird = new FlappyBird();
+      frame.add(flappyBird);
+      flappyBird.requestFocus();
+      frame.pack();
+      frame.setVisible(true);
+   }
 }
